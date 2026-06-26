bd7dc8fa25cf91fbf5b1f53602426714051b90a9
JavaDeveloper-Sinku <sinkusingh173@gmail.com>
2026-06-26 16:47:28 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/rishi/drms/DistributorRetailerManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rishi.drms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributorRetailerManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributorRetailerManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rishi/drms/DistributorRetailerManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishi.drms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributorRetailerManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
